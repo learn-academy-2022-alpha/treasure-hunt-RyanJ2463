32aba76941eb933107015c5fb745f854681c3578
RyanJ2463 <jacobellr2017@gmail.comjacobellr2017@gmail.comcd>
2022-03-08 17:37:54 -0500

    treasure hunt

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
 import './App.css'
+import Square from './components/Square'
 
 class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      board: ["?", "?", "?", "?", "?", "?", "?", "?", "?"]
+      board: ["?", "?", "?", "?", "?", "?", "?", "?", "?"],
+      treasureLocation: ""
+    }
+  }
+
+componentDidMount(){
+  let treasure = Math.floor(Math.random() *
+  this.state.board.length)
+  this.setState({treasureLocation: treasure})
+}
+
+  handleGamePlay = (index) => {
+    const { board, treasureLocation } = this.state
+    if(index === treasureLocation){
+      board[index] = "❤️"
+      this.setState({board: board})
+    }else{
+      board[index] = "🌴"
+      this.setState({board: board})
     }
   }
 
@@ -13,6 +32,17 @@ class App extends Component{
     return(
       <>
         <h1>Treasure Hunt Game</h1>
+        <div id="gameboard">
+          {this.state.board.map((value, index) => {
+            return (
+              <Square
+                value={value} 
+                index={index}
+                handleGamePlay={this.handleGamePlay}
+             />
+            )
+          })}
+        </div>
       </>
     )
   }
